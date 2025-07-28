2e3844b39ba5ece6302399e4073d1146bfab40f2
个不想起名的人 某 <No_Namer@NoNameTeamCN.onmicrosoft.com>
2025-07-28 16:34:28 +0800

    Create category.

---
+++ My First C++ Application.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+
+void CPrintf(){}
+
+int main()
+{
+	system("title No Namers' Minigames 0.0.1");
+	system("color 02");
+	cout << R"(                            (c)
+       XXXXXXXXXXXXXXXXXXX
+       XXXXXXXXXXXXXXXXXXX
+              XXXXX
+XXXXX  XX XX  XXXXX      
+XXXXXX XX XX  XXXXX      
+XXXXXXXXX XX  XXXXX      
+XXXXXXXXX XX  XXXXX      
+XXXXX XXX XX  XXXXX      
+XXXXX  XXXXX  XXXXX      
+XXXXX   XXXX  XXXXX      
+         XXX  XXXXX      
+          XXX XXXXX      
+XXXXX      XXXXXXXX      
+XXXXXX      XXXXXXX      
+XXXXXXX      XXXXXX      
+XXXXXXXX                 
+XXXXX XXX                
+XXXXX  XXX    XXXXX      
+XXXXX   XXX   XXXXX      
+XXXXX    XXX  XXXXX      
+XXXXX     XXX XXXXX      
+XXXXX      XXXXXXXX      
+XXXXX       XXXXXXX      
+XXXXX        XXXXXX)" << endl;
+	cout << "\nCopyright No Name Team 2025 all rights reserved." << endl;
+	system("pause");
+	system("cls");
+	cout << "No Namers' Minigame 0.0.1" << endl;
+	cout << "This is a debug version,nothing can be sured to add in the final version and the program may crash anytime." << endl;
+	cout << "Exiting..." << endl;
+    return 0;
+}
